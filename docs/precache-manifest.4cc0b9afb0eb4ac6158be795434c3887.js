8ef875a291eaf4218f910a3739479b2da2f9e2bf
avnerkhan <avnerkhan@utexas.edu>
2020-01-09 17:59:07 -0600

    Added docs for hosting

---
+++ docs/precache-manifest.4cc0b9afb0eb4ac6158be795434c3887.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c40e0f0528b1d03d272d88e335160cd8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f1f6256290fd4eea6517",
+    "url": "/static/css/2.96479aa2.chunk.css"
+  },
+  {
+    "revision": "9d0e78de0baa241efb11",
+    "url": "/static/css/main.085ed047.chunk.css"
+  },
+  {
+    "revision": "f1f6256290fd4eea6517",
+    "url": "/static/js/2.8e21857d.chunk.js"
+  },
+  {
+    "revision": "9388aab11bfa68c0a6f39348352d4706",
+    "url": "/static/js/2.8e21857d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9d0e78de0baa241efb11",
+    "url": "/static/js/main.aa8d3cb5.chunk.js"
+  },
+  {
+    "revision": "9950a0670e71ea8787b2",
+    "url": "/static/js/runtime-main.e5767198.js"
+  }
+]);
